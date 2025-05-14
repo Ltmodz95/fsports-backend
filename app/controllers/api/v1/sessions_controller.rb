f83809d77d484853270267ee282608c20da3bfa7
mohamed abdelmoniem hussein <mohamed.elsayed@procore.com>
2025-05-14 13:09:35 +0300

    Handeling 401

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,8 @@
 module Api
   module V1
     class SessionsController < BaseController
-      allow_unauthenticated_access only: %i[ create ]
+      allow_unauthenticated_access only: %i[ create verify ]
       rate_limit to: 10, within: 3.minutes, only: :create, with: -> { render json: { error: "Too many attempts. Try again later." }, status: :too_many_requests }
-
       def create
         if user = User.authenticate_by(params.permit(:email_address, :password))
           start_new_session_for user
@@ -14,7 +13,7 @@ module Api
       end
 
       def verify
-        if Current.session.token == params[:session]
+        if Session.where(token: params[:token]).exists?
           render json: { data: { token: Current.session.token } }
         else
           render json: { error: "Invalid token" }, status: :unauthorized
